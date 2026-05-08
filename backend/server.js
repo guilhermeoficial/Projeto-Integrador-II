f7c99e4b4c5d61f7142ff707bdcbc1b06c960bdd
guilhermeoficial <64939169+guilhermeoficial@users.noreply.github.com>
2026-05-08 11:12:43 -0300

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+backend/server.js
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        mensagem: 'API Sistema de Reservas funcionando'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
